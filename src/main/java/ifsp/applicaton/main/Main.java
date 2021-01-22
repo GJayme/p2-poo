5d401026fa02fb298b364d6116a582ee896d5753
Gabriel Jayme <gabrielj_89@hotmail.com>
2021-01-22 17:56:38 -0300

    feat: injection usecases on main

---
+++ src/main/java/ifsp/applicaton/main/Main.java
@@ -0,0 +1,31 @@
+package ifsp.applicaton.main;
+
+import ifsp.domain.usercases.pessoa.CreatePessoaUseCase;
+import ifsp.domain.usercases.pessoa.DeletePessoaUseCase;
+import ifsp.domain.usercases.pessoa.ReadPessoaUseCase;
+import ifsp.domain.usercases.pessoa.UpdatePessoaUseCase;
+import ifsp.domain.usercases.venda.CreateVendaUseCase;
+import ifsp.domain.usercases.venda.DeleteVendaUseCase;
+import ifsp.domain.usercases.venda.ReadVendaUseCase;
+import ifsp.domain.usercases.venda.UpdateVendaUseCase;
+
+public class Main {
+
+    public static CreatePessoaUseCase createPessoaUseCase;
+    public static DeletePessoaUseCase deletePessoaUseCase;
+    public static ReadPessoaUseCase readPessoaUseCase;
+    public static UpdatePessoaUseCase updatePessoaUseCase;
+
+    public static CreateVendaUseCase createVendaUseCase;
+    public static DeleteVendaUseCase deleteVendaUseCase;
+    public static ReadVendaUseCase readVendaUseCase;
+    public static UpdateVendaUseCase updateVendaUseCase;
+
+    public static void main(String[] args) {
+        configureInjection();
+    }
+
+    private static void configureInjection() {
+
+    }
+}
