bdcfe50adbd66a7e9b6380ace51ef454e7adfe93
Gabriel Jayme <gabrielj_89@hotmail.com>
2021-01-25 17:36:20 -0300

    feat: create ConnectionFactory

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module ifsp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires sqlite.jdbc;
 
     opens ifsp.applicaton.view to javafx.fxml;
     opens ifsp.applicaton.controller to javafx.fxml;
